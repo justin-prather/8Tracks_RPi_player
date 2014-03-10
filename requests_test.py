62c8c67afdc597c30dfbaeede5c2a3bab932e9ef
Justin Prather <justinprather@mac.com>
2014-03-10 14:40:29 -0700

    happy days its sunny in van

--- requests_test.py
+++ requests_test.py
@@ -2,3 +2,12 @@ import urllib2
 import urllib
 import json
 import time
+
+api_key = 'ef1b85bdb35b68b0f7ce0f7d6a575c528e600405'
+
+print 'Developer Auth'
+
+url = urllib2.urlopen('http://8tracks.com/mixes/new.json?api_key='+api_key+'&api_version=3')
+json_response = json.load(url)
+print json_response
+

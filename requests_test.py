a28c6aeed72163c28c3d5a8d67c84c0dced3a015
Justin Prather <justinprather@mac.com>
2014-03-11 19:34:38 -0700

    making good progress understanding the api

--- requests_test.py
+++ requests_test.py
@@ -5,9 +5,32 @@ import time
 
 api_key = 'ef1b85bdb35b68b0f7ce0f7d6a575c528e600405'
 
+user_data = { 'login':'justin.prather1', 'password':'camerasrule', 
+	'api_version':'3'}
+
 print 'Developer Auth'
 
 url = urllib2.urlopen('http://8tracks.com/mixes/new.json?api_key='+api_key+'&api_version=3')
 json_response = json.load(url)
 print json_response
 
+print 'User Auth'
+
+user_data_encoded = urllib.urlencode(user_data)
+
+user_request = urllib2.Request( 'https://8tracks.com/sessions.json', user_data_encoded)
+
+response = urllib2.urlopen(user_request)
+
+user_json = json.load(response)
+print user_json
+print user_json[unicode('user')][unicode('user_token')]
+
+print 'Searching mixes'
+
+mixes = urllib2.urlopen('http://8tracks.com/mix_sets/all.json?include=mixes&api_key='
+	+api_key+'api_version=3')
+json_mixes = json.load(mixes)
+
+for mix in json_mixes[unicode('mix_set')][unicode('mixes')]:
+	print mix[unicode('name')]

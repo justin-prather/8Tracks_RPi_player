4f48fa5cc7f5da7652936ac18807f362fd9ad06d
Justin Prather <justinprather@mac.com>
2014-03-29 19:12:22 -0700

    playing with image layout

--- image_view.py
+++ image_view.py
@@ -16,9 +16,13 @@ screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
 url = urllib2.urlopen('http://8tracks.imgix.net/i/001/974/875/the-1975-live-matty-9393.jpg?fm=jpg&q=65&w=250&h=250&fit=crop')
 image_file = StringIO.StringIO(url.read())
 
-pic  = pygame.image.load(image_file)
+Next  = pygame.image.load("Next.png")
+back = pygame.image.load("back.png")
+cover = pygame.image.load(image_file)
 while True:
-	screen.fill((255,0,0))
-	screen.blit(pic, (35, 0))	
+	screen.fill((0xC0, 0xC0, 0xC0))
+	screen.blit(Next, (320-35, 0))
+	screen.blit(back, (0,0))
+	screen.blit(cover, (35, 0))
 	pygame.display.update()
 

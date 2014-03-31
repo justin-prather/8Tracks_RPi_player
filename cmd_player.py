2f742baf21df61a1ed0c8ff3ba89d777fd38dd61
Justin Prather <justinprather@mac.com>
2014-03-31 11:50:28 -0700

    changed cmd_player to include multiple searches

--- cmd_player.py
+++ cmd_player.py
@@ -43,54 +43,62 @@ def play():
 				break
 		player.currentMix_json = player.get_similar_mix()
 
+#======================================================================================
+
 player = Gr8_Tracks( 'ef1b85bdb35b68b0f7ce0f7d6a575c528e600405', 'justin.prather1',
 			     'camerasrule')
 
 player.get_play_token()
 
-search_type = str(raw_input("Enter search type (all, tags, keyword): "))
-print search_type
-tags = None
-if search_type == "tags":
-	tags = str(raw_input("Enter tags separated by spaces: ")).split()
+while True:
+	search_type = str(raw_input("Enter search type (all, tags, keyword): "))
+	print search_type
+	tags = None
+	if search_type == "tags":
+		tags = str(raw_input("Enter tags separated by spaces: ")).split()
 
-elif search_type == "keywords":
-	tags = str(raw_input("Enter keywords separated by spaces: ")).split()
+	elif search_type == "keywords":
+		tags = str(raw_input("Enter keywords separated by spaces: ")).split()
 
-search_filter = str(raw_input("Enter filter (recent, all, popular): "))
+	search_filter = str(raw_input("Enter filter (recent, all, popular): "))
 
-search_results = player.search_mix(search_type, keys=tags, sort=search_filter)
+	search_results = player.search_mix(search_type, keys=tags, sort=search_filter)
 
-for i in range(0,len(search_results[unicode('mix_set')][unicode('mixes')])):
-	print str(i) + ') ' + search_results[unicode('mix_set')][unicode('mixes')][i][unicode('name')].encode('utf-8')
+	for i in range(0,len(search_results[unicode('mix_set')][unicode('mixes')])):
+		print str(i) + ') ' + search_results[unicode('mix_set')][unicode('mixes')][i][unicode('name')].encode('utf-8')
 
-print 'Enter mix number:'
-player.currentMix_json = search_results[unicode('mix_set')][unicode('mixes')][int(raw_input())]
+	print 'Enter mix number:'
+	if is_playing:
+		is_playing = False
+		t.join()
+	player.currentMix_json = search_results[unicode('mix_set')][unicode('mixes')][int(raw_input())]
 
-t = threading.Thread( target = play )
-t.start()
+	t = threading.Thread( target = play )
+	t.start()
 
-try:
-	while True:
-		user_input = str(raw_input("Enter a command: "))
-		
-		if user_input == "pause":
-			toggle_pause = True
-		elif user_input == 'next':
-			next = True
-		elif user_input == 'exit':
+		try:
+			while True:
+				user_input = str(raw_input("Enter a command: "))
+				
+				if user_input == "pause":
+					toggle_pause = True
+				elif user_input == 'next':
+					next = True
+				elif user_input == 'search':
+					break
+				elif user_input == 'exit':
+					is_playing = False
+					t.join()
+					import sys
+					sys.exit()
+				else:
+					print "Not a valid command"
+		except KeyboardInterrupt:
 			is_playing = False
 			t.join()
 			import sys
 			sys.exit()
-		else:
-			print "Not a valid command"
-except KeyboardInterrupt:
-	is_playing = False
-	t.join()
-	import sys
-	sys.exit()
-#play(player)
+
 
 
 

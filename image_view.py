41546fdeb4c20de5d9ce28ed72ef82cccc7c2c0f
Justin Prather <justinprather@mac.com>
2014-05-09 18:34:38 -0600

    started working on laptop instead of pi. life is easier

--- image_view.py
+++ image_view.py
@@ -4,14 +4,15 @@ from pygame.locals import *
 import urllib2
 import StringIO
 
-os.putenv('SDL_VIDEODRIVER', 'fbcon')
-os.putenv('SDL_FBDEV'      , '/dev/fb1')
-os.putenv('SDL_MOUSEDRV'   , 'TSLIB')
-os.putenv('SDL_MOUSEDEV'   , '/dev/input/touchscreen')
+# os.putenv('SDL_VIDEODRIVER', 'fbcon')
+# os.putenv('SDL_FBDEV'      , '/dev/fb1')
+# os.putenv('SDL_MOUSEDRV'   , 'TSLIB')
+# os.putenv('SDL_MOUSEDEV'   , '/dev/input/touchscreen')
 
 pygame.init()
-pygame.mouse.set_visible(False)
-screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+#pygame.mouse.set_visible(False)
+#screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+screen = pygame.display.set_mode([320,240])
 
 url = urllib2.urlopen('http://8tracks.imgix.net/i/001/974/875/the-1975-live-matty-9393.jpg?fm=jpg&q=65&w=250&h=250&fit=crop')
 image_file = StringIO.StringIO(url.read())
